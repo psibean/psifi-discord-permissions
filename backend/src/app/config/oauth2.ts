29806cac1a04e3c420899deda546f965ff4f79c1
psibean <psionatix@gmail.com>
2022-12-16 22:25:06 +1030

    Add guilds scope to oauth2

--- backend/src/app/config/oauth2.ts
+++ backend/src/app/config/oauth2.ts
@@ -6,7 +6,7 @@ const {
   confirmAuthorization,
   oauth2
 } = OAuth2PKCE({
-  scopes: ['identify'],
+  scopes: ['identify', 'guilds'],
   oauthClientOptions: {
     authorizeUrl:  "/oauth2/authorize",
     baseUrl: "https://discord.com",
