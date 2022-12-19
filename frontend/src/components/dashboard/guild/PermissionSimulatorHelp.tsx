72c2358e4905f4c546aa6b4c83a75ac195a1aa72
psibean <psionatix@gmail.com>
2022-12-19 19:38:06 +1030

    Update guide

--- frontend/src/components/dashboard/guild/PermissionSimulatorHelp.tsx
+++ frontend/src/components/dashboard/guild/PermissionSimulatorHelp.tsx
@@ -16,14 +16,16 @@ export default () => {
         <Paragraph>
           Select a channel to view the permissions available on the channel based on the roles you have selected on the left hand side. The permissions you see here are the permissions a user would have given the selected roles, it is all calculated based on your servers base permissions, category and channel specific overrides. Calculations are made as per the official <Anchor href="https://discord.com/developers/docs/topics/permissions#permission-overwrites">Discord documentation pseudocode</Anchor>.
         </Paragraph>
-        <Paragraph>If any of the roles you have selected has the Administrator permission, then all permissions will be available.</Paragraph>
+        <Paragraph>
+          Please note there are other things which may impact what a user can or cannot do. For example, if a user A and user B both have the Administrator permission, they are still unable to take certain actions against the server owner, such as mute and kick. They will also be unable to take certain actions against other administrator's whose highest ranking role is higher than their own in the role hierarchy.
+        </Paragraph>
         <HeaderTwo>Guide</HeaderTwo>
         <UnorderedList>
           <ListItem>
             If a channel is not listed, the bot does not have permission to view it and cannot show you permissions for it.
           </ListItem>
           <ListItem>
-            If a role provides the Administrator permission, then a user will have ALL permissions. If you do not want the simulator to show all permissions when the Administrator permission is available, turn off the Administrator toggle on the left.
+            If a role provides the Administrator permission, then a user will have ALL permissions.
           </ListItem>
           <ListItem>
             If a channel is crossed out and cannot be clicked, it means that channel cannot be viewed based on the current roles, and thus, no other permissions matter.
