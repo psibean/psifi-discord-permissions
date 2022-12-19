c7829415cccdf1a7efec0a4494bc630d31ad8893
psibean <psionatix@gmail.com>
2022-12-19 19:40:15 +1030

    Add admin warning

--- frontend/src/components/dashboard/guild/ChannelPermissions.tsx
+++ frontend/src/components/dashboard/guild/ChannelPermissions.tsx
@@ -4,22 +4,38 @@ import ChannelPermission from "./ChannelPermission";
 import { useCalculatedMemberPermissionsForChannel } from "../../../state/simulatedServer.slice";
 import ChannelIcon from "./ChannelIcon";
 import { BaseSimulatorPageBody, BaseSimulatorPageContainer, BaseSimulatorPageHeader } from "./BaseSimulatorPage";
+import { PermissionFlagsBits } from "discord-api-types/v10";
+import { useMemo } from "react";
+import Paragraph from "../../common/Paragraph";
 
 export type ChannelPermissionsProps = {
   channel: SelectedGuildChannel;
   permissions: PsifiPermission[];
 }
 
+const AdministratorPermissionWarning = () => {
+  return (
+    <div className="bg-orange-400">
+      <Paragraph className="text-center text-black font-medium">Warning: One of the roles you have selected has the Administrator permission, this is the same as having all permissions.</Paragraph>
+    </div>
+  )
+}
+
 export default ({ channel, permissions }: ChannelPermissionsProps) => {
   const memberChannelPermissions = useCalculatedMemberPermissionsForChannel(channel.id);
-  
+  const hasAdmin = useMemo(
+    () => hasPermission(PermissionFlagsBits.Administrator, memberChannelPermissions), 
+    [memberChannelPermissions]
+  );
+
   return (<BaseSimulatorPageContainer>
     <BaseSimulatorPageHeader>
-      Permissions for  <ChannelIcon className="inline-block h-8 w-8 mr-1" type={channel.type} />{ channel.name }
+      Permissions for <ChannelIcon className="inline-block h-8 w-8 mr-1" type={channel.type} />{ channel.name }
     </BaseSimulatorPageHeader>
+    { hasAdmin && <AdministratorPermissionWarning /> }
     <BaseSimulatorPageBody>
       {
-        permissions.map(permission => <ChannelPermission key={`${channel.id}-channel-permission-${permission.bitfield}}`} permission={permission} enabled={hasPermission(permission.bitfield, BigInt(memberChannelPermissions))} />)
+        permissions.map(permission => <ChannelPermission key={`${channel.id}-channel-permission-${permission.bitfield}}`} permission={permission} enabled={hasPermission(permission.bitfield, memberChannelPermissions)} />)
       }
     </BaseSimulatorPageBody>
     </BaseSimulatorPageContainer>)
